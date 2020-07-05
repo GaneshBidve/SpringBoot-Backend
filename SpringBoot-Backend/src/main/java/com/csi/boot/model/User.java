dc865229f24f094d0048d860622bdb3dc355a791
Ganesh_Bidve <ganeshbidve07@gmail.com>
2020-07-05 12:21:02 +0530

    User class

--- SpringBoot-Backend/src/main/java/com/csi/boot/model/User.java
+++ SpringBoot-Backend/src/main/java/com/csi/boot/model/User.java
@@ -1,5 +1,34 @@
 package com.csi.boot.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "email")
+	private String email;
 
 }
